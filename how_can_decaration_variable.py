4d0958e37f8710f5b511a9edc583197cb2fa28d3
Om prakash sah teli <155549636+23AD094OP@users.noreply.github.com>
2024-03-10 16:46:11 +0530

    Add files via upload

---
+++ how_can_decaration_variable.py
@@ -0,0 +1,33 @@
+a = 5
+b = 2.5
+c = 4000076576765165
+d = 34678673585235876.4151
+e = 'c'
+f = "string sentences or words"
+g = '''AI & DS'''
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(f)
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
+print(type(g))
+# 5
+# 2.5
+# 4000076576765165
+# 3.4678673585235876e+16
+# c
+# string sentences or words
+# <class 'int'>
+# <class 'float'>
+# <class 'int'>
+# <class 'float'>
+# <class 'str'>
+# <class 'str'>
+# <class 'str'>
