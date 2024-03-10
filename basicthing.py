b8a6a6dacc01654814fd8f8e3b552d9cc4eb08e2
OM PRAKASH SAH TELI <omprakashsah4044@gmail.com>
2024-03-10 16:04:00 +0530

    done

---
+++ basicthing.py
@@ -0,0 +1,15 @@
+print("Hello")
+x = "hello om prakash"
+print(x)
+a = 2+5
+print(a)
+a = int(input("Enter the first number:- "))
+b = int(input("Enter the second number:- "))
+x = a + b
+print(x)
+# Hello
+# hello om prakash
+# 7
+# Enter the first number:- 4
+# Enter the second number:- 6
+# 10
